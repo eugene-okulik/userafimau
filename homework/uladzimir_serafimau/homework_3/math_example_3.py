c213073f173c021ef56f14aa71dcf10668103290
Uladzimir Serafimau <userafimau@ibagroup.eu>
2024-02-15 13:44:37 +0300

    homework 3 fixing error 2 line

--- homework/uladzimir_serafimau/homework_3/math_example_3.py
+++ homework/uladzimir_serafimau/homework_3/math_example_3.py
@@ -2,7 +2,7 @@ num1 = 2
 num2 = 3
 
 average_arithmetic = (num1 + num2) / 2
-average_geometric = (num1 * num2) ** (1/2)
+average_geometric = (num1 * num2) ** (1 / 2)
 
 print(average_arithmetic)
 print(average_geometric)
--- homework/uladzimir_serafimau/homework_3/math_example_4.py
+++ homework/uladzimir_serafimau/homework_3/math_example_4.py
@@ -3,7 +3,7 @@ cathetus_b = 12
 
 area = (cathetus_a * cathetus_b) / 2
 
-hypotenuse = (cathetus_a**2 + cathetus_b**2) ** (1/2)
+hypotenuse = (cathetus_a**2 + cathetus_b**2) ** (1 / 2)
 
 print('Area:', area)
 print('Hypotenuse', hypotenuse)
