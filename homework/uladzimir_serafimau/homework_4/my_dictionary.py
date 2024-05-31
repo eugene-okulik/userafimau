968c7be8aedc3f4a2174ef65314d9072855e2116
Uladzimir Serafimau <userafimau@ibagroup.eu>
2024-05-31 15:43:11 +0300

    homework 4 done - dictionary

---
+++ homework/uladzimir_serafimau/homework_4/my_dictionary.py
@@ -0,0 +1,25 @@
+my_dict = {'tuple': (1, 2, 3, 'text', 2.56),
+           'list': [4, 6, 8, 'example', 5.298],
+           'dict': {6: 1, 7: 2, 8: 3, 9: 4, 10: 5},
+           'set': {9, 0, 10.29, True, 'last'}}
+
+# Для того, что храниться под ключом 'tuple'
+
+print(my_dict['tuple'][-1])
+
+# Для того, что хранится под ключом 'list'
+
+my_dict['list'].append('last elemet')
+del my_dict['list'][1]
+
+# Для того, что хранится под ключом 'dict'
+
+my_dict['dict']['i am a tuple'] = 555
+del my_dict['dict'][7]
+
+# Для того, что хранится под ключом 'set'
+
+my_dict['set'].add('another elemet')
+my_dict['set'].remove(True)
+
+print(my_dict)
